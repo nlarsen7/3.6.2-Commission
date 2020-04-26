742df3a3c017cf5cff256757a8050bfb3eb35afd
nlarsen7 <nlarsen2013@gmail.com>
2020-04-26 02:19:26 +0000

    3.6.2 Commission

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+//Nicholas Larsen
+//4/25/2020
+//finds out the commission of a total price
 package main
 
 import "fmt"
@@ -6,17 +9,21 @@ func commission(small float64, medium float64, large float64)float64{
 var total float64
 fmt.Println("what is the total value of your home?")
 fmt.Scanln(&total)
+//asks for the total value
 if total<50000{
   return small*total
 }else if total>50000 && total<=250000{
   return medium*total
 }else{
   return large*total
+  //returns whichever value based on the total
 }
 }
 func main () {
 small:=.04
 medium:=.06
 large:=.0675
+//commission rates
 fmt.Println("your commission total is",(commission(small, medium, large)))
+//your commission total
 }
